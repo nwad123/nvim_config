0623f9fa0861fa38fbe1ffc7234cc68b40171172
Nick Waddoups <nick.waddoups@usu.edu>
2024-09-11 13:04:53 -0600

    updated which key bindings to modern version

--- lua/plugins/whick-key.lua
+++ lua/plugins/whick-key.lua
@@ -9,23 +9,42 @@ return {
     },
     config = function()
         local wk = require("which-key")
-        wk.register({
-            f = {
-                name = "file",
-                n = { "<cmd>new<cr>", "New files" },
-                w = { "<cmd>w<cr>", "Write file" },
+        wk.add({
+            {
+                -- Base keybinds
+                mode = { "n", "v" },
+                { "<leader>q", "<cmd>q<cr>", desc = "quit" },
+                {
+                    "<leader>b",
+                    group = "buffers",
+                    expand = function()
+                        return require("which-key.extras").expand.buf()
+                    end
+                },
             },
-            l = { name = "lsp" },
-            w = {
-                name = 'window',
-                s = { "<cmd>split<cr>", "split" },
-                v = { "<cmd>vsplit<cr>", "vertical split" },
-                c = { "<cmd>wq!<cr>", "write and close" },
-                h = { "<cmd>wincmd h<cr>", "move left" },
-                j = { "<cmd>wincmd j<cr>", "move down" },
-                k = { "<cmd>wincmd k<cr>", "move up" },
-                l = { "<cmd>wincmd l<cr>", "move right" },
+            {
+                -- File keybindings
+                mode = { "n", "v" },
+                { "<leader>f",  group = "file" },
+                { "<leader>fn", "<cmd>new<cr>", desc = "new file" },
+                { "<leader>fw", "<cmd>w<cr>", desc = "write" },
             },
-        }, { prefix = "<leader>" })
+            {
+                -- LSP keybindings
+                mode = { "n", "v" },
+                { "<leader>l", group = "lsp" },
+            },
+            {
+                -- Window keybindings
+                mode = { "n", "v" },
+                { "<leader>w",  group = "window" },
+                { "<leader>ws", "<cmd>split<cr>",    desc = "split" },
+                { "<leader>wv", "<cmd>vsplit<cr>",   desc = "vertical split" },
+                { "<leader>wh", "<cmd>wincmd h<cr>", desc = "move left" },
+                { "<leader>wj", "<cmd>wincmd j<cr>", desc = "move down" },
+                { "<leader>wk", "<cmd>wincmd k<cr>", desc = "move up" },
+                { "<leader>wl", "<cmd>wincmd l<cr>", desc = "move right" },
+            },
+        })
     end
 }
