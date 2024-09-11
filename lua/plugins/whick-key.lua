33effa258afa3036498a003a38b19a7a717cfe61
Nick Waddoups <nick.waddoups@usu.edu>
2024-09-11 12:35:52 -0600

    Revert "Updated which key config to modern style"
    This reverts commit 32f925b114ec12a9c5ab7d7541aa779e99320c53.

--- lua/plugins/whick-key.lua
+++ lua/plugins/whick-key.lua
@@ -9,20 +9,23 @@ return {
     },
     config = function()
         local wk = require("which-key")
-        wk.register(
-            {
-                { "<leader>f",  group = "file" },
-                { "<leader>fn", "<cmd>new<cr>",      desc = "New files" },
-                { "<leader>fw", "<cmd>w<cr>",        desc = "Write file" },
-                { "<leader>l",  group = "lsp" },
-                { "<leader>w",  group = "window" },
-                { "<leader>wc", "<cmd>wq!<cr>",      desc = "write and close" },
-                { "<leader>wh", "<cmd>wincmd h<cr>", desc = "move left" },
-                { "<leader>wj", "<cmd>wincmd j<cr>", desc = "move down" },
-                { "<leader>wk", "<cmd>wincmd k<cr>", desc = "move up" },
-                { "<leader>wl", "<cmd>wincmd l<cr>", desc = "move right" },
-                { "<leader>ws", "<cmd>split<cr>",    desc = "split" },
-                { "<leader>wv", "<cmd>vsplit<cr>",   desc = "vertical split" },
-            })
+        wk.register({
+            f = {
+                name = "file",
+                n = { "<cmd>new<cr>", "New files" },
+                w = { "<cmd>w<cr>", "Write file" },
+            },
+            l = { name = "lsp" },
+            w = {
+                name = 'window',
+                s = { "<cmd>split<cr>", "split" },
+                v = { "<cmd>vsplit<cr>", "vertical split" },
+                c = { "<cmd>wq!<cr>", "write and close" },
+                h = { "<cmd>wincmd h<cr>", "move left" },
+                j = { "<cmd>wincmd j<cr>", "move down" },
+                k = { "<cmd>wincmd k<cr>", "move up" },
+                l = { "<cmd>wincmd l<cr>", "move right" },
+            },
+        }, { prefix = "<leader>" })
     end
 }
