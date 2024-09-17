a27830e7d8bae419740481a2f592dcb71266f731
Nick Waddoups <nick.waddoups@usu.edu>
2024-09-17 13:04:42 -0600

    Added clangd to lsp config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -15,7 +15,8 @@ return {
         keys = {
             { "<leader>lh", function () vim.lsp.buf.hover() end, desc = "Hover" },
             { "<leader>la", function () vim.lsp.buf.code_action() end, desc = "Show code actions" },
-            { "<leader>ld", function () vim.lsp.buf.definition() end, desc = "Go to defition" }
+            { "<leader>ld", function () vim.lsp.buf.definition() end, desc = "Go to defition" },
+            { "<leader>lr", function () vim.lsp.buf.definition() end, desc = "Rename" },
         },
         config = function()
             local lspconfig = require("lspconfig")
@@ -23,6 +24,7 @@ return {
             lspconfig.rust_analyzer.setup({})
             lspconfig.pyright.setup({})
             lspconfig.ts_ls.setup({})
+            lspconfig.clangd.setup({})
         end
     }
 }
