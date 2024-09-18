aacdd3788272a44b2934c5739feacb1611962cfc
Nick Waddoups <nick.waddoups@usu.edu>
2024-09-17 20:02:10 -0600

    Fixed rename support

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -16,11 +16,7 @@ return {
             { "<leader>lh", function () vim.lsp.buf.hover() end, desc = "Hover" },
             { "<leader>la", function () vim.lsp.buf.code_action() end, desc = "Show code actions" },
             { "<leader>ld", function () vim.lsp.buf.definition() end, desc = "Go to defition" },
-<<<<<<< HEAD
-            { "<leader>lr", function () vim.lsp.buf.definition() end, desc = "Rename" },
-=======
             { "<leader>lr", function () vim.lsp.buf.rename() end, desc = "Rename" },
->>>>>>> 8eb37f8 (Added rename support)
         },
         config = function()
             local lspconfig = require("lspconfig")
