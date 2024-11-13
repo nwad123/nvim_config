c8f2a5ce9744a5080306168133d9a2dbeca4c77e
Nick Waddoups <nick.waddoups@usu.edu>
2024-11-13 13:15:19 -0700

    Added command for extra diagnostic window

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,22 +1,36 @@
 return {
-	{
+    {
         "williamboman/mason.nvim",
         config = true
     },
-	{
+    {
         "williamboman/mason-lspconfig.nvim",
         opts = {
             ensure_installed = { "lua_ls", "rust_analyzer", "pyright", "ts_ls" },
         },
         config = true
     },
-	{
+    {
         "neovim/nvim-lspconfig",
         keys = {
-            { "<leader>lh", function () vim.lsp.buf.hover() end, desc = "Hover" },
-            { "<leader>la", function () vim.lsp.buf.code_action() end, desc = "Show code actions" },
-            { "<leader>ld", function () vim.lsp.buf.definition() end, desc = "Go to defition" },
-            { "<leader>lr", function () vim.lsp.buf.rename() end, desc = "Rename" },
+            { "<leader>lh", function() vim.lsp.buf.hover() end,        desc = "Hover" },
+            { "<leader>la", function() vim.lsp.buf.code_action() end,  desc = "Show code actions" },
+            { "<leader>ld", function() vim.lsp.buf.definition() end,   desc = "Go to defition" },
+            { "<leader>lr", function() vim.lsp.buf.rename() end,       desc = "Rename" },
+            {
+                "<leader>lm",
+                function()
+                    vim.diagnostic.open_float(nil, {
+                        focusable = false,
+                        close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+                        border = 'rounded',
+                        source = 'always',
+                        prefix = ' ',
+                        scope = 'cursor',
+                    })
+                end,
+                desc = "Diagnostic messages"
+            },
         },
         config = function()
             local lspconfig = require("lspconfig")
