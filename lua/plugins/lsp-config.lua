03265af7963f9cc73fe5a1fdb7eab75372018157
Nick Waddoups <nick.waddoups@usu.edu>
2024-11-14 11:13:51 -0700

    Added see declaration

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -16,6 +16,7 @@ return {
             { "<leader>lh", function() vim.lsp.buf.hover() end,       desc = "Hover" },
             { "<leader>la", function() vim.lsp.buf.code_action() end, desc = "Show code actions" },
             { "<leader>ld", function() vim.lsp.buf.definition() end,  desc = "Go to defition" },
+            { "<leader>lD", function() vim.lsp.buf.declaration() end, desc = "Go to declaration" },
             { "<leader>lr", function() vim.lsp.buf.rename() end,      desc = "Rename" },
             {
                 "<leader>lm",
