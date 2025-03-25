6503564687c515281e3f2dce47cb1fc4df5df18a
Nick Waddoups <nick.waddoups@usu.edu>
2025-03-25 11:09:00 -0600

    formatted lua

--- lua/nick/plugins/lsp/lsp-config.lua
+++ lua/nick/plugins/lsp/lsp-config.lua
@@ -111,11 +111,11 @@ return {
 				return orig_util_open_floating_preview(contents, syntax, opts, ...)
 			end
 
-            -- iterate over all possible lsp servers and set them up
-            -- TODO: I know we can do something with 'on_attach' but that's for another day :)
-		    for _, v in pairs(lspconfig.util.available_servers()) do
-		        lspconfig[v].setup({capabilities = capabilities})
-		    end
+			-- iterate over all possible lsp servers and set them up
+			-- TODO: I know we can do something with 'on_attach' but that's for another day :)
+			for _, v in pairs(lspconfig.util.available_servers()) do
+				lspconfig[v].setup({ capabilities = capabilities })
+			end
 		end,
 	},
 }
