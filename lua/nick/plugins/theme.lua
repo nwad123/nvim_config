8056ecda3d577eadc929b05e02cc93ae6ec7b19a
Nick Waddoups <nick.waddoups@usu.edu>
2025-04-03 10:34:34 -0600

    Removed old theme

--- lua/nick/plugins/theme.lua
+++ lua/nick/plugins/theme.lua
@@ -1,25 +1,3 @@
--- return {
---	"folke/tokyonight.nvim",
---   	lazy = false, -- make sure we load this during startup if it is your main colorscheme
---	priority = 1000, -- make sure to load this before all the other start plugins
---	config = function()
---	    -- load the colorscheme here
---	    vim.cmd.colorscheme "tokyonight-storm"
---  	end,
---}
-
--- return {
---     "ellisonleao/gruvbox.nvim",
---     priority = 10,
---     config = function()
---         require("gruvbox").setup({
---             contrast = "hard",
---         })
---
---         vim.cmd.colorscheme "gruvbox"
---     end
--- }
-
 return {
 	"rebelot/kanagawa.nvim",
 	priority = 10,
