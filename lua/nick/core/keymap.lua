74b43698123a3a12312148bf1ba1a595badb5fca
Nick Waddoups <nick.waddoups@usu.edu>
2025-04-03 11:09:00 -0600

    Added other tab operations

--- lua/nick/core/keymap.lua
+++ lua/nick/core/keymap.lua
@@ -2,4 +2,7 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.keymap.set("n", " ", "<Nop>")
-vim.keymap.set("n", "T", "<cmd>tabn<cr>")
+
+-- Tab movement
+vim.keymap.set("n", "T", "<cmd>tabnext<cr>")
+vim.keymap.set("n", "t", "<cmd>1tabnext<cr>")
