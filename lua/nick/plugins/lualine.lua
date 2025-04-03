b9f0cdad3c1c6ee6948bc478160136c1bbc0253e
Nick Waddoups <nick.waddoups@usu.edu>
2025-04-03 11:01:27 -0600

    Updated lualine to be more nicer :)

--- lua/nick/plugins/lualine.lua
+++ lua/nick/plugins/lualine.lua
@@ -1,15 +1,29 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-        icons_enabled = false,
-        theme = "everforest",
-        sections = {
-            lualine_a = { "mode" },
-            lualine_b = { "branch", "diff", "diagnostics" },
-            lualine_c = { "filename" },
-        },
-        disabled_filetypes = { "neo-tree" },
-    },
-    config = true,
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		options = {
+			section_separators = { left = "", right = "" },
+			component_separators = { left = "|", right = "|" },
+			theme = "iceberg_dark",
+		    disabled_filetypes = { "neo-tree" },
+		},
+		sections = {
+			lualine_a = {},
+			lualine_b = { "branch", "diff", "diagnostics" },
+			lualine_c = {},
+			lualine_x = {},
+			lualine_y = { "lsp_status" },
+			lualine_z = { "mode" },
+		},
+		inactive_sections = {
+			lualine_a = {},
+			lualine_b = { "filename" },
+			lualine_c = {},
+			lualine_x = {},
+			lualine_y = {},
+			lualine_z = {},
+		},
+	},
+	config = true,
 }
